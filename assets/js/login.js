232f5d63b2221900f69b0ce9e84246f724c555e9
ronlee <840522056@qq.com>
2021-01-30 15:21:32 +0800

    完成首页开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,25 +1,29 @@
-$(function () {
+$(function() {
   // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function () {
+  $('#link_reg').on('click', function() {
     $('.login-box').hide()
     $('.reg-box').show()
   })
+
   // 点击“去登录”的链接
-  $('#link_login').on('click', function () {
+  $('#link_login').on('click', function() {
     $('.login-box').show()
     $('.reg-box').hide()
   })
 
-  // 自定义校验规则
-  let form = layui.form
-  let layer = layui.layer
+  // 从 layui 中获取 form 对象
+  var form = layui.form
+  var layer = layui.layer
   // 通过 form.verify() 函数自定义校验规则
   form.verify({
-    // 自定义 pwd 密码校验规则
+    // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 确认密码的 校验规则
-    repwd: function (value) {
-      // 选择类名为reg-box元素 包裹的 name属性为password
+    // 校验两次密码是否一致的规则
+    repwd: function(value) {
+      // 通过形参拿到的是确认密码框中的内容
+      // 还需要拿到密码框中的内容
+      // 然后进行一次等于的判断
+      // 如果判断失败,则return一个提示消息即可
       var pwd = $('.reg-box [name=password]').val()
       if (pwd !== value) {
         return '两次密码不一致！'
@@ -27,23 +31,27 @@ $(function () {
     }
   })
 
-  // 发起注册请求
-  $('#form_reg').on('submit', function (e) {
+  // 监听注册表单的提交事件
+  $('#form_reg').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
     e.preventDefault()
-
-    $.post('/api/reguser', {
+    // 2. 发起Ajax的POST请求
+    var data = {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
-    }, function (res) {
-      if (res.status !== 0) return layer.msg(res.message);
-
-      layer.msg(res.message);
+    }
+    $.post('/api/reguser', data, function(res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请登录！')
+      // 模拟人的点击行为
       $('#link_login').click()
     })
   })
 
-  // 发起登录请求
-  $('#form_login').submit(function (e) {
+  // 监听登录表单的提交事件
+  $('#form_login').submit(function(e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
@@ -51,9 +59,11 @@ $(function () {
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
-      success(res) {
-        if (res.status !== 0) return layer.msg('登录失败！')
-
+      success: function(res) {
+        console.log(res);
+        if (res.status !== 0) {
+          return layer.msg('登录失败！')
+        }
         layer.msg('登录成功！')
         // 将登录成功得到的 token 字符串，保存到 localStorage 中
         localStorage.setItem('token', res.token)
